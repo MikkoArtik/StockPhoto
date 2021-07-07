0c89607150de09ed12be71a3cb706955f84a28e7
Michael <mihail.tchernov@yandex.ru>
2021-07-07 08:46:38 +0300

    Проект завершен, ожидает ревью

--- server.py
+++ server.py
@@ -1,14 +1,12 @@
 import os
-import asyncio
-from datetime import datetime
-from aiohttp import web
-
-import aiofiles
 import logging
+from datetime import datetime
+import signal
+import argparse
 
-
-ROOT_FOLDER = '/home/michael/Документы/Education/AsyncPython/StockPhoto/' \
-              'async-download-service/test_photos'
+import asyncio
+from aiohttp import web
+import aiofiles
 
 
 async def handle_index_page(request):
@@ -23,8 +21,8 @@ async def handle_404_page(request):
     return web.Response(text=content, content_type='text/html')
 
 
-async def archivate(request):
-    target_folder = os.path.join(ROOT_FOLDER,
+async def archiving(request):
+    target_folder = os.path.join(os.getenv('root_folder'),
                                  request.match_info['hash_value'])
     if not os.path.exists(target_folder):
         return await handle_404_page(request)
@@ -43,13 +41,23 @@ async def archivate(request):
 
     logging.info('Start sending...')
 
-    while True:
-        data = await proc.stdout.read(1024 * 5)
-        if not data:
-            break
+    try:
+        while True:
+            data = await proc.stdout.read(1024 * 5)
+            if not data:
+                break
 
-        await response.write(data)
-        logging.info('Sending archive chunk ...')
+            try:
+                await response.write(data)
+                logging.info('Sending archive chunk ...')
+                await asyncio.sleep(float(os.getenv('delay')))
+            except asyncio.CancelledError:
+                proc.send_signal(signal.SIGKILL)
+                logging.warning('Download was interrupted')
+                break
+    except SystemExit:
+        proc.send_signal(signal.SIGKILL)
+        _ = proc.communicate()
     return response
 
 
@@ -63,15 +71,27 @@ async def uptime_handler(request):
         message_text = datetime.now().strftime('%d-%m-%Y %H:%M:%S<br>')
         await response.write(message_text.encode('UTF-8'))
 
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(float(os.getenv('delay')))
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser(
+        description='Проект микросервиса для загрузки фотографий')
+    parser.add_argument('root_folder', help='Корневая папка с фотографиями')
+    parser.add_argument('delay', help='Задержка сервера')
+    parser.add_argument('--log', help='Включение журнала логирования')
+
+    args = parser.parse_args()
+    if args.log == 'ON':
+        logging.basicConfig(level=logging.DEBUG)
+
+    os.environ['root_folder'] = args.root_folder
+    os.environ['delay'] = args.delay
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{hash_value}/', archivate),
+        web.get('/archive/{hash_value}/', archiving),
         web.get('/uptime', uptime_handler)
     ])
     web.run_app(app)
